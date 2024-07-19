338848fcd077b9040913c2b5a3167f50ade5722c
shivani <shivaninimmy@gmail.com>
2024-07-19 11:23:24 -0400

    commit

--- web-crawler/src/main/java/com/example/service/CoahraneCrawlerService.java
+++ web-crawler/src/main/java/com/example/service/CoahraneCrawlerService.java
@@ -3,9 +3,49 @@
  */
 package com.example.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 
  */
+@service
 public class CoahraneCrawlerService {
-
+	
+	final String baseurl="https://www.cochranelibrary.com/cdsr/reviews/topics";
+	final String outputfile="C:/Users/shiva/eclipse-workspace/Web-Crawler/cochrane_reviews.txt";
+    
+	public reviews ReadReviewsurl(String baseurl) {
+		//List the review urls from the coachrane library
+		List reviewurls=getreviewUrls();
+		//Hold the reviews objects
+		List reviews=new ArrayList<>();
+		
+		for(String reviewurl:reviewurls) {
+			reviews.add(getreviewDetails(reviewurl));
+		}
+		writeReviewsToFile(reviews);
+		
+		
+	}
+	private void writeReviewsToFile(List reviews) {
+		// TODO Auto-generated method stub
+		
+	}
+	private List getreviewUrls() {
+		Document doc=jsoup.conn(baseurl):
+			
+		return null;
+	}
+	private review getreviewDetails(String reviewurl) {
+		
+		return null;
+	}
+	}
+	
+	
+	
+	
+	
+    
 }
